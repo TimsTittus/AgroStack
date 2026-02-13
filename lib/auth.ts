25eb4b6840dddd41e24e6cae55ca2f831a7e0316
Abin Thomas <followgamer102@gmail.com>
2026-02-13 10:15:09 +0530

    next-app with trpc better-auth setup

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/index";
+import * as schema from "@/db/schema";
+
+export const auth = betterAuth({
+  trustedOrigins: ["*"],
+  advanced: {
+    cookiePrefix: "agroo",
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  sendOnSignUp: true,
+  autoSignInAfterVerification: true,
+  expiresIn: 3600,
+});
