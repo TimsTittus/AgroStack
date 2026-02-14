b3c54f4e183dd189a13a85c0b1009e62303414eb
Aravind Manoj <aravindmng47@gmail.com>
2026-02-14 12:24:11 +0530

    fix: db prodcuts moved to listings

--- db/relations.ts
+++ db/relations.ts
@@ -1,17 +1,17 @@
 import { relations } from "drizzle-orm/relations";
-import { user, account, session, products, orders } from "./schema";
+import { user, account, session, listings, orders } from "./schema";
 
-export const accountRelations = relations(account, ({one}) => ({
+export const accountRelations = relations(account, ({ one }) => ({
 	user: one(user, {
 		fields: [account.userId],
 		references: [user.id]
 	}),
 }));
 
-export const userRelations = relations(user, ({many}) => ({
+export const userRelations = relations(user, ({ many }) => ({
 	accounts: many(account),
 	sessions: many(session),
-	products: many(products),
+	listings: many(listings),
 	orders_buyerId: many(orders, {
 		relationName: "orders_buyerId_user_id"
 	}),
@@ -20,22 +20,22 @@ export const userRelations = relations(user, ({many}) => ({
 	}),
 }));
 
-export const sessionRelations = relations(session, ({one}) => ({
+export const sessionRelations = relations(session, ({ one }) => ({
 	user: one(user, {
 		fields: [session.userId],
 		references: [user.id]
 	}),
 }));
 
-export const productsRelations = relations(products, ({one, many}) => ({
+export const listingsRelations = relations(listings, ({ one, many }) => ({
 	user: one(user, {
-		fields: [products.userid],
+		fields: [listings.userid],
 		references: [user.id]
 	}),
 	orders: many(orders),
 }));
 
-export const ordersRelations = relations(orders, ({one}) => ({
+export const ordersRelations = relations(orders, ({ one }) => ({
 	user_buyerId: one(user, {
 		fields: [orders.buyerId],
 		references: [user.id],
@@ -46,8 +46,8 @@ export const ordersRelations = relations(orders, ({one}) => ({
 		references: [user.id],
 		relationName: "orders_farmerId_user_id"
 	}),
-	product: one(products, {
+	product: one(listings, {
 		fields: [orders.productId],
-		references: [products.id]
+		references: [listings.id]
 	}),
 }));
